4548defd67efd0ef4c77f12074373f65c328a4d8
Mickey Barboi <damouse007@gmail.com>
2015-10-27 22:13:52 -0500

    Initial Commit

---
+++ SwiftNodeBench/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftNodeBench
+//
+//  Created by Mickey Barboi on 10/27/15.
+//  Copyright © 2015 paradrop. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
